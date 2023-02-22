01932e472a9621dddfeedcd4ebe009fc9d06886a
Malcev-Nikita <mnd1work@gmail.com>
2023-02-22 11:52:49 +0400

    commit 22.2.23 (1)

--- App.js
+++ App.js
@@ -9,48 +9,38 @@ import Settings from './src/panels/Settings';
 
 import tab from './src/style/tab_style';
 
-const homeName = "Home";
-const detailsName = "Details";
-const settingsName = "Settings";
+const homeName = "Домой";
+const settingsName = "Настройки";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-          <Tab.Navigator 
-            initialRouteName={homeName}
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-                let rn = route.name;
-    
-                if (rn === homeName) {
-                  iconName = focused ? 'home' : 'home-outline';
-    
-                } 
-                // else if (rn === detailsName) {
-                //   iconName = focused ? 'list' : 'list-outline';
-    
-                // } 
-                // else if (rn === settingsName) {
-                //   iconName = focused ? 'settings' : 'settings-outline';
-                // }
-    
-                // You can return any component that you like here!
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-            })}
-            tabBarOptions={{
-              activeTintColor: '#A78093',
-              inactiveTintColor: '#E0D3CE',
-              labelStyle: { paddingBottom: 7, fontSize: 10 },
-              style: { padding: 10, height: 70, }
-            }}
-          >
-              <Tab.Screen name={homeName} component={Home} options={{headerShown: false}} />
-              <Tab.Screen name="Settings" component={Settings} options={{headerShown: false}} />
-          </Tab.Navigator>
+    <NavigationContainer style={{ borderRadius: 10 }}>
+      <Tab.Navigator 
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) iconName = focused ? 'home' : 'home-outline'; 
+
+            else if (rn === settingsName) iconName = focused ? 'settings' : 'settings-outline';
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#A78093',
+          inactiveTintColor: '#E1BBAD',
+          labelStyle: { paddingBottom: 48, fontSize: 10 },
+          tabStyle: {  paddingTop: 3, height: 90, backgroundColor: '#F2E5DF' }
+        }}
+      >
+          <Tab.Screen name={homeName} component={Home} options={{headerShown: false}} />
+          <Tab.Screen name={settingsName} component={Settings} options={{headerShown: false}} />
+      </Tab.Navigator>
       </NavigationContainer>
   );
 }
