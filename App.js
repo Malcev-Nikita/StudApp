0821df33b1a4f8b64e389116a15a48bae41908c0
Никита <mnd1work@gmail.com>
2023-02-25 00:05:10 +0400

    commit 25.2.23 (1)

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import Icons from 'react-native-vector-icons/Feather';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Home from './src/panels/Home';
 import Settings from './src/panels/Settings';
-
-import tab from './src/style/tab_style';
 
 const homeName = "Домой";
 const settingsName = "Настройки";
@@ -16,31 +12,35 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer style={{ borderRadius: 10 }}>
+    <NavigationContainer>
       <Tab.Navigator 
-        initialRouteName={homeName}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            let rn = route.name;
-
-            if (rn === homeName) iconName = focused ? 'home' : 'home-outline'; 
-
-            else if (rn === settingsName) iconName = focused ? 'settings' : 'settings-outline';
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: '#A78093',
-          inactiveTintColor: '#E1BBAD',
-          labelStyle: { paddingBottom: 48, fontSize: 10 },
-          tabStyle: {  paddingTop: 3, height: 90, backgroundColor: '#F2E5DF' }
+        screenOptions={{ 
+          headerShown: false,
+          tabBarStyle: {
+            height: 55,
+            position: 'absolute',
+            bottom: 16,
+            right: 25,
+            left: 25,
+            borderRadius: 16 }
         }}
+        tabBarOptions={{showLabel: false}}
+        initialRouteName={homeName}
+        labeled={false}
       >
-          <Tab.Screen name={homeName} component={Home} options={{headerShown: false}} />
-          <Tab.Screen name={settingsName} component={Settings} options={{headerShown: false}} />
+          <Tab.Screen name={homeName} component={Home} options={{
+            headerShown: false, 
+            tabBarIcon: ({ color }) => (
+              <Icons name="home" color={color} size={26} />
+            ) }} 
+          />
+          <Tab.Screen name={settingsName} component={Settings} options={{
+            headerShown: false, 
+            tabBarIcon: ({ color }) => (
+              <Icons name="settings" color={color} size={26} />
+            ) }} 
+          />
       </Tab.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
   );
 }
