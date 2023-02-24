449cf214108e8b5e326779a6812b4c942e0232f1
Никита <mnd1work@gmail.com>
2023-02-25 01:38:41 +0400

    commit 25.2.23 (2)

---
+++ src/components/Avatar.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { Text, TouchableOpacity, View, Image } from 'react-native';
+
+import Settings from './Settings';
+
+import global from '../style/global_style';
+import home from '../style/home_style';
+
+
+class Avatar extends React.Component {
+  render() {
+    return (
+        <View >
+
+        </View>
+    );
+  };
+};
+
+export default Avatar;
