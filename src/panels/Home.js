840ae08810c476047a2965fda0e050923f431c08
Malcev-Nikita <mnd1work@gmail.com>
2023-03-01 23:21:03 +0400

    commit 1.3.23 (1)

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,20 +1,76 @@
 
 import React from 'react';
-import { Text, TouchableOpacity, View, Image } from 'react-native';
+import { Text, TouchableOpacity, View, Image, SafeAreaView, ScrollView } from 'react-native';
 
 import Settings from './Settings';
 
 import global from '../style/global_style';
 import home from '../style/home_style';
 
-
 class Home extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://a15459-a752.s.d-f.pw/api/get_all_items_of_group/1")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    return (
-        <View>
-            <Text>Домашняя страница</Text>
-        </View>
-    );
+    const { error, isLoaded, items } = this.state;
+
+    onchange = (nativeEvent) => {
+
+    }
+
+    if (error) return <Text style={home.mt}>Ошибка: {error.message}</Text>
+
+    else if (!isLoaded) return <Text style={home.mt}>Загрузка...</Text>
+
+    else {
+      return (
+          <View>
+            <SafeAreaView style={home.container}>
+              <View style={home.wrap}>
+                <ScrollView 
+                  onScroll={({nativeEvent}) => onchange(nativeEvent)}
+                  showsHorizontalScrollIndicator={false}
+                  pagingEnabled
+                  horizontal
+                  style={home.wrap}
+                >
+                  {
+                    items.map( item => (
+                      <Text style={home.mt}>{item.Id_Call_Schedule}</Text>
+                    ))
+                  }
+                </ScrollView>
+              </View>
+            </SafeAreaView>
+          </View>
+      );
+    }
   };
 };
 
--- src/style/home_style.js
+++ src/style/home_style.js
@@ -1,9 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 const home = StyleSheet.create({
 
-    
-    
+    container: {
+        flex: 1,
+    },
+
+    mt: {
+        marginTop: 100
+    },
+
+    wrap: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height * 0.25,
+        backgroundColor: '#a8a8a8'
+    }
+
 });
 
 export default home
